148d63ef780773a15c63ee7a9d2a03f64b089a04
phucdang18 <phucdang18@utexas.edu>
2018-11-17 04:13:13 -0600

    some test cases for deep copy the object

---
+++ src/SaveObjectTest.java
@@ -0,0 +1,34 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SaveObjectTest {
+    private static MonitorLock lock = new MonitorLock();
+
+    @Test
+    public void testSuperField() {
+        TestSerialize test = new TestSerialize();
+        int expected = test.getNum();
+        lock.lock(test);
+        test.setNum(100);
+        lock.abort();
+        Assert.assertEquals(test.getNum(), expected);
+    }
+
+    @Test
+    public void testOwnField() {
+        TestSerialize test = new TestSerialize();
+        int expected = test.aNum;
+        lock.lock(test);
+        test.aNum = 20;
+        lock.abort();
+        Assert.assertEquals(test.aNum, expected);
+    }
+
+
+
+
+
+
+
+
+}
---
+++ src/TestSerialize.java
@@ -0,0 +1,11 @@
+public class TestSerialize extends SmallerObject{
+
+    int aNum = 0;
+    CustomObject object;
+
+    public TestSerialize() {
+        object = new CustomObject();
+    }
+
+
+}
