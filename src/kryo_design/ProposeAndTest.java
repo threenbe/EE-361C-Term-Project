69acbbd37b98b001a72769b2bfc1802c7c00a936
Ashkan Vafaee <ashkanvafaee@utexas.edu>
2018-11-18 13:10:55 -0600

    Description added to ProposeAndTest

--- src/kryo_design/ProposeAndTest.java
+++ src/kryo_design/ProposeAndTest.java
@@ -1,3 +1,23 @@
+/*
+SIZE threads are created. Each thread has a local "proposals" array of integers
+of size 20. Each of these arrays are initialized slightly differently. For example 
+Thread 0's array has a 0 at index 0 and 0 everywhere else. Thread 1
+has a 1 at index 1 and 0 everywhere else. Thread 2 has 2 at index 2 and 0 everywhere
+else etc.
+
+There are SIZE rounds that all of the threads must go through. During each round,
+all threads concurrently write to the global "answer" array at index "index" using
+their proposed value at proposals[index]. In order for the threads to "pass" a round, 
+Thread i must be the last to write to answer[i] during round i. For example, during 
+round 1, thread 1 should be the last to write to answer[1] so that answer[1] ends up
+having the value 1. For round 2, thread 2 should be the last to write to answer[2] so
+that answer[2] ends up having the value 2 etc.
+
+If the correct thread is the last to write its proposal value to the answers array
+then index is incremented and we can move on to the next round. Else, we must abort
+and try again.
+*/
+
 package kryo_design;
 
 import java.util.ArrayList;
